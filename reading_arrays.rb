f9ab5a0507745033d3ee7710906e408e1dad01d9
jackybethea1234 <jackybethea@gmail.com>
2018-07-11 14:40:32 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -13,11 +13,13 @@ def first_student_by_index
 end
 puts first_student_by_index
 
+
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
   STUDENT_NAMES[3]
 end
 puts fourth_student_by_index
+
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
@@ -25,11 +27,13 @@ def last_student_by_index
 end
 puts last_student_by_index
 
+
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
   STUDENT_NAMES.first
 end
 puts first_student_by_method
+
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
@@ -37,10 +41,10 @@ def last_student_by_method
 end
 puts last_student_by_method
 
+
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
   STUDENT_NAMES[0][1][2]
 end
-
-puts first_second_and_third_students
+print first_second_and_third_students
 
